22a6b8dd6fd620820fc00311a016a61ff6830ea4
Heirch <149302547+Heirch@users.noreply.github.com>
2024-05-10 19:27:13 +0530

    Add files via upload

---
+++ resources/uag-js-25.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-8ee1fc8d' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-da320b7c' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-e1c7968f' );
+});
+ });
